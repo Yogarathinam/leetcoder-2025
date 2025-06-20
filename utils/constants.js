62f7a0d01231e29640ff2e728fe9d743dd59ac6c
Yogarathinam <yogarathinam26@gmail.com>
2025-06-20 13:59:33 +0530

    Updated Leetcode Bot

---
+++ utils/constants.js
@@ -0,0 +1,40 @@
+// SCRAPERs XPATH
+export const SCRAPER_SUBMITTED_CODE_NAME_XPATH = "/html/body/div[2]/div/div[1]/div/div[1]/h4/a";
+export const SCRAPER_SUBMITTED_CODE_LANGUAGE_XPATH =
+  "/html/body/div[2]/div/div[1]/div/div[2]/div[7]/div/div[1]/div/div[1]/span";
+export const SCRAPER_SUBMITTED_CODE_DIV_XPATH =
+  "/html/body/div[2]/div/div[1]/div/div[2]/div[7]/div/div[3]/div/div/div[3]/div/div[3]";
+
+  export const QUESTIONS_LANGUAGE_BTN_SELECTOR = 'button[aria-haspopup="dialog"]';
+  export const QUESTIONS_LANGUAGE_DIV_SELECTOR = 'div.text-text-primary.relative.text-sm';
+  export const QUESTIONS_CODE_EDITOR_SELECTOR = 'div.view-lines';
+  export const QUESTIONS_SUBMIT_BTN_SELECTOR = 'button[data-e2e-locator="console-submit-button"]';
+  export const IS_SOLUTION_ACCEPTED_TEXT = 'Accepted';
+  
+
+
+export const LEETCODER_ASCII_ART = `
+    __                   __               __           
+    | |                  | |              | |          
+    | |     ___  ___  ___| |_ ___ ___   __| | ___ _ __ 
+    | |    / _ \\/ _ \\/ _ \\ __/ __/ _ \\ / _| |/ _ \\ '__|
+    | |___|  __/  __/  __/ || (_| (_) | (_| |  __/ |   
+    \\_____/\\___|\\___|\\___|\\__\\___\\___/ \\__,_|\\___|_|  
+
+             Leetcoder 2025 — Automated Submissions
+        Modified & Maintained by: Yogarathinam T L
+       Based on original project by: @chanpreet3000
+         Repo: https://github.com/Yogarathinam/leetcoder-2025
+    `;
+
+export const LEETCODER_MODE_QUESTION = `
+    Choose an action
+    [1] Run Leetcode Bot
+    [2] Scrape Solved LeetCode Problems
+    [other] Exit
+    `;
+
+export const EXITING_LEETCODER = `Thank you for using Leetcoder 2025!
+GitHub: https://github.com/Yogarathinam/leetcoder-2025
+Based on original project by: @chanpreet3000
+Please use responsibly.`;
